527839a9170659204a7cac0b19e948b0c79e9511
Mark Rassamni <markrassamni@me.com>
2018-11-19 19:36:07 -0800

    Initial Commit

---
+++ CS635Assignment4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS635Assignment4
+//
+//  Created by Mark Rassamni on 11/19/18.
+//  Copyright © 2018 Mark Rassamni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CS635Assignment4Tests/CS635Assignment4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CS635Assignment4Tests.swift
+//  CS635Assignment4Tests
+//
+//  Created by Mark Rassamni on 11/19/18.
+//  Copyright © 2018 Mark Rassamni. All rights reserved.
+//
+
+import XCTest
+@testable import CS635Assignment4
+
+class CS635Assignment4Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
