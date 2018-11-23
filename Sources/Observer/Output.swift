da4846adb1204fe10bfc7058bae8bd43b9b75a98
Mark Rassamni <markrassamni@me.com>
2018-11-22 21:09:33 -0800

    starter code to send email

--- Sources/Observer/Output.swift
+++ Sources/Observer/Output.swift
@@ -7,10 +7,29 @@
 //
 
 import Foundation
+import SwiftSMTP
 
 class Output {
     
-    func sendEmail(to address: String, message: String){
+    func sendEmail(to user: Mail.User, message: String) {
+        let smtp = SMTP(
+            hostname: "smtp.gmail.com",
+            email: "user@gmail.com",
+            password: "password"
+        )
+        let sender = Mail.User(name: "Mark Rassamni", email: "markrassamni@gmail.com")
         
+        let mail = Mail(
+            from: sender,
+            to: [user],
+            subject: "WebPage Changed",
+            text: message
+        )
+        // TODO: Remove closure and do not show error
+        smtp.send(mail) { (error) in
+            if let error = error {
+                print(error)
+            }
+        }
     }
 }
