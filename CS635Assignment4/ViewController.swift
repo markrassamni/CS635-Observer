6b5a773294f2e4c435f0614a22d285e568d2ddff
Mark Rassamni <markrassamni@me.com>
2018-11-25 22:12:32 -0800

    using web page subject to store all (subject url date) instead of context

--- CS635Assignment4/ViewController.swift
+++ CS635Assignment4/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
     
     let testURL = "https://dzone.com/articles/6-reasons-why-you-should-go-for-a-static-website"
-
+//    enum DateError: LocalizedError{
+//        case noDate(String)
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        test2()
+        
+//        let error = "The operation couldn’t be completed."
+//        let error2 = "The operation couldn’t be completed.".localizedDescription
+//        let a = DateError.noDate("Could not retrieve date from header field.")
+//        let b = DateError.noDate("Could not retrieve date from header field.").localizedDescription
+//        print(error)
+//        print(error2)
+//        let a = DateError(description: "Error")
+//        let b = a .localizedDescription
+//
+//        print(a)
+//        print(b)
+
+        
+//        let a = DateError(description: "A")
+//        let b = DateError.noDate
+//        let c = DateError.noDate.localizedDescription
+        
+//        print(b)
+//        
+//        print(c)
+        
+        
+        testHeaders()
     }
     
     func test1(){
-        let a = "https://dzone.com/articles/6-reasons-why-you-should-go-for-a-static-website"
+        let _ = "https://dzone.com/articles/6-reasons-why-you-should-go-for-a-static-website"
         let b = "http://www.eli.sdsu.edu/courses/fall18/cs635/notes/index.html"
         Factory().getDateModified(of: b) { (date) in
             guard let date = date else { return }
@@ -31,7 +57,32 @@ class ViewController: UIViewController {
     func test2(){
         Factory().testObserve(url: testURL)
     }
+    
+    func testHeaders(){
+        Alamofire.request("http://www.eli.sdsu.edu/courses/fall18/cs635/notes/index.html").responseString { (response) in
+            switch response.result{
+            case .failure(let error):
+                //                self.subject.onError(error)
+                print(error.localizedDescription)
+            case .success:
+                let data = response.response?.allHeaderFields
+                print(data ?? "No data")
+                guard let date = response.response?.allHeaderFields["Date"] as? String else { return }
+                print(date)
+                print("Hi")
+                ()
+                //                self.subject.onNext(date)
+            }
+        }
+    }
 
 
 }
 
+//fileprivate struct DateError: LocalizedError {
+//    private var description: String
+//
+//    var errorDescription: String? {
+//        return description
+//    }
+//}
--- CS635Assignment4/WebPageObserver.swift
+++ CS635Assignment4/WebPageObserver.swift
@@ -10,20 +10,36 @@ import Foundation
 import RxSwift
 import Alamofire
 
-struct WebPageObserver: Hashable {
+class WebPageObserver: Hashable { // TODO: Rename ...subject
+    
     let subject: PublishSubject<String>
     let url: String
+    var dateModified: String
     
-    func checkForUpdates(url: String, dateUpdated: @escaping (String?)->()){
-        Alamofire.request(url).responseJSON { (response) in
-            switch response.result{
-            case .failure(let error):
+    init(subject: PublishSubject<String>, url: String, dateModified: String) {
+        self.subject = subject
+        self.url = url
+        self.dateModified = dateModified
+    }
+    
+    func checkForUpdates(){
+        ConnectionHandler.instance.getDateModified(for: self) { (error, date) in
+            if let error = error {
                 self.subject.onError(error)
-            case .success:
-                guard let date = response.response?.allHeaderFields["Date"] as? String else { return }
-                // TODO: Compare to previous date
+            } else if let date = date {
                 self.subject.onNext(date)
             }
         }
+    }
+    
+    // TODO: Remove hashable?
+    static func == (lhs: WebPageObserver, rhs: WebPageObserver) -> Bool {
+        return lhs.subject == rhs.subject && lhs.url == rhs.url && lhs.dateModified == rhs.dateModified
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(subject)
+        hasher.combine(url)
+        hasher.combine(dateModified)
     }
 }
